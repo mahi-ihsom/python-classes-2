94916ff666a7e798ab7921496fe771d1464eaa82
mahi-ihsom <devendrajoshi80@gmail.com>
2025-07-15 17:16:57 +0400

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+kirti="6/1/2012"
+anushka="7/7/2017"
+mahi="30/7/2014"
+rythm="9/7/2013"
+simon="28/11/2013"
+print("Kirti's birthday is on", kirti)
+print("Anushka's birthday is on", anushka)
+print("my birthday is on", mahi)
+print("Rythm's birthday is on", rythm)
+print("Simon's birthday is on", simon)

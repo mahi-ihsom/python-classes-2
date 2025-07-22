7f4e7d9e9c64f4edcd7a8ac72dcbd90cfb30a8c5
mahi-ihsom <devendrajoshi80@gmail.com>
2025-07-22 17:18:47 +0400

    pythonchallenges

---
+++ python_challenges.py
@@ -0,0 +1,16 @@
+#activity1
+#part1
+v1= 4
+v2= 5
+v3= 8
+v4= 2
+v5= 0
+v5= (v1+v2)*v3/v4
+print(v5)
+#part2
+name= "Mahi"
+age= 0
+if name=="Alex" or name=="Mahi" and age>=2:
+    print("Hello, welcome")
+else:
+    print("Goodbye.")

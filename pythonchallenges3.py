17f6bd0f73b9f2614d48a8dfb9b1a7eb02cf42bc
mahi-ihsom <devendrajoshi80@gmail.com>
2025-07-23 13:37:06 +0400

    initial commit

--- pythonchallenges3.py
+++ pythonchallenges3.py
@@ -1,5 +1,14 @@
 #activity3
-mean=38
+mean1=38
 inc_num=36
 cor_num=56
-total=40
+total_number=40
+#sum of 40 number
+sum= mean1*total_number
+print("the sum of 40 numbers:", sum)
+#correct sum of these numbers
+num2=sum-((inc_num)-(cor_num))
+print("sum-((inc_num)-(correct number)):", 2)
+#the correct mean
+mean2= num2/total_number
+print(mean2)

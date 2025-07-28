7e1ebc37aeb27d9029a478829a016b3ceeef688e
mahi-ihsom <devendrajoshi80@gmail.com>
2025-07-28 17:21:58 +0400

    nestedloop1

---
+++ actvity1_11.py
@@ -0,0 +1,9 @@
+ui=(input("Enter thine word:  "))
+ui2= (input("Enter your own character:  "))
+i= 0
+count= 0
+while (i<len(ui)):
+    if (ui[i]==ui2):
+        count= count+1
+    i= i+1
+print("The total number of times", ui2, "has occured=", count)

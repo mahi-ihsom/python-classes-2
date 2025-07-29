b624855446f5ccf9a179531aa4d29061cf3a6075
mahi-ihsom <devendrajoshi80@gmail.com>
2025-07-29 17:19:57 +0400

    int.com

--- actvity1_11.py
+++ actvity1_11.py
@@ -6,4 +6,4 @@ while (i<len(ui)):
     if (ui[i]==ui2):
         count= count+1
     i= i+1
-print("The total number of times", ui2, "has occured=", count)
+print("The total number of times", ui2, "has occured=", )

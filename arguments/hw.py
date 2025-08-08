ba2d6beb025ee0a5da97b9487ad48ef8b1a0e259
mahi-ihsom <devendrajoshi80@gmail.com>
2025-08-08 17:23:15 +0400

    initial commit

---
+++ arguments/hw.py
@@ -0,0 +1,15 @@
+green= "Chrome"
+blue= "Edge"
+red= "WhatsApp"
+def questioni(x):
+    if x=="yes" or x=="YES" or x=="Yes":
+        return "Okay"
+    else:
+        return "Shut down and try again"
+def questionii(y):
+    if y=="yes" or y=="YES" or y=="Yes":
+        return "okay"
+    else:
+        return "Shut down and try again"
+def questioniii(z):
+    if z=="yes" or z=="YES" or z=="Yes"

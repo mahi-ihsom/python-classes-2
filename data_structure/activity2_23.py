cdfbc823044438a04ebfa91d63f57eee3f877b47
mahi-ihsom <devendrajoshi80@gmail.com>
2025-08-18 17:42:51 +0400

    xyz

---
+++ data_structure/activity2_23.py
@@ -0,0 +1,9 @@
+#set intersection
+set_x={"Green","Blue"}
+set_y={"Blue", "Yellow"}
+print("Original set element:")
+print(set_x)
+print(set_y)
+print("\nIntersection of two sets:")
+set_z= set_x.intersection(set_y)
+print(set_z)

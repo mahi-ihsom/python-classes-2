265d4444a4cc72a3b8501fdbf002c3657ab35a82
mahi-ihsom <devendrajoshi80@gmail.com>
2025-08-25 17:55:24 +0400

    initial commit

---
+++ hw_symetrical_diff.py
@@ -0,0 +1,11 @@
+set1= {"Blue", "Green"}
+set2= {"Yellow", "Green"}
+def amazing_function():
+    set1.symmetric_difference(set2)
+print(amazing_function())
+
+set1b= {1,2,3,4,5}
+set2b= {1,5,6,7,8,9}
+def thingy():
+    set1b.symmetric_difference(set2b)
+print(amazing_function())

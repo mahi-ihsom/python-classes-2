f78b3082be2bc982e6d240c2a4fdaeaab45cd653
mahi-ihsom <devendrajoshi80@gmail.com>
2025-09-05 19:26:52 +0400

    jk

--- data_structure/lists_hw.py
+++ data_structure/lists_hw.py
@@ -8,48 +8,61 @@ ui5= int(input())
 list= [ui1, ui2, ui3, ui4, ui5]
 if ui1%2!=0:
         print(ui1, "is odd")
-        ui1="odd"
+        odd= True
 else:                
     print(ui1, "is even.")
+    odd= False
 if ui2%2!=0:
     print(ui2, "is odd.")
     odd= True
 else:
     print(ui2, "is even")
+    odd= False
 if ui3%2!=0:
     print(ui3, "is odd.")
-    ui3="odd"
+    odd= True
 else:
     print(ui3, "is even")
+    odd= False
 if ui4%2!=0:
     print(ui4,"is odd.")
-    ui4= "odd"
+    odd= True
 else:
    print(ui4, "is even.")
+   odd= False
 if ui5%2!=0:
     print(ui5, "is odd.")
-    ui5= "odd"
+    odd= True
 else:
     print(ui5, "is even.")
+    odd= False
 
-if ui1=="odd":
+if ui1==odd:
     odd_list= [ui1]
-    if ui2=="odd":
+    if ui2==odd:
         odd_list=[ui1, ui2]
-        if ui3=="odd":
+        if ui3==odd:
             odd_list=[ui1,ui2,ui3]
-    if ui3=="odd":
+    if ui3==odd:
         odd_list= [ui1,ui3]
-elif ui2=="odd":
+elif ui2==odd:
     odd_list= [ui2]
-    if ui3=="odd":
+    if ui3==odd:
         odd_list=[ui2, ui3]
-        if ui4=="odd":
+        if ui4==odd:
             odd_list=[ui2,ui3,ui4]
-elif ui3=="odd":
+elif ui3==odd:
     odd_list= [ui3]
+    if ui4==odd:
+        odd_list= [ui3, ui4]
+    if ui5==odd:
+        odd_list=[ui3, ui4, ui5]
+elif ui4==odd:
+    odd_list=[ui4]
+    if ui5==odd:
+        odd_list= [ui4, ui5]
+elif ui5==odd:
+    odd_list= [ui5]
 else:
-    print("ue")
-
-
+    print("INPUT INVALID")
 print(odd_list)
---
+++ function/thing.py
@@ -0,0 +1,11 @@
+lpk= "String"
+j= 9
+if 'S' in lpk:
+    s= 'S'
+    t= 't'
+    r= 'r'
+    i= 'i'
+    n= 'n'
+    g= 'g'
+print(s,t,r,i,n,g)
+print(g,n,i,r,t,s)

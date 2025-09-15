aec37c552a4643a2551b3805e4645addaf53cf55
mahi-ihsom <devendrajoshi80@gmail.com>
2025-09-15 17:26:35 +0400

    github

---
+++ tkinter/activity1_37.py
@@ -0,0 +1,17 @@
+#number pad
+from tkinter import *
+root= Tk()
+root.title("number pad")
+root.geometry("250x300")
+num= [[9,8,7], [6,5,4], [3,2,1], ["#",0,"*"]]
+
+for i in range(4):
+    root.columnconfigure(i,weight=1, minsize= 75)
+    root.rowconfigure(i,weight=1, minsize= 50)
+for i in range(4):
+    for j in range(3):
+        frame= Frame(master=root, relief=SUNKEN, borderwidth=1, bg= "#fcbee4")
+        frame.grid(row=i, column= j, sticky= "nsew")
+        label= Label(master=frame, text=num[i][j],bg="White", font=("Times new roman", 18))
+        label.pack(expand=True, fill="both", padx= 5, pady=5)
+root.mainloop()
